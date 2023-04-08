a259b7988599a00ef6856084b9af00fb5ed54e87
thesquarerootofthree <98823947+thesquarerootofthree@users.noreply.github.com>
2023-04-08 23:05:38 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("shabby")
+})
+music.playMelody("E G E G A A A G ", 120)
+music.playMelody("A G G D D D D C ", 120)
+music.playMelody("D E E E D E D D ", 120)
+music.playMelody("C C C D E F F F ", 120)
+music.playMelody("F F G D D D C D ", 120)
+music.playMelody("C C C C C C - - ", 120)
